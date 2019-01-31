427a772e47df47c51f669fb8af8f27aee07f6d03
usman.siddiqui96 <usman624@gmail.com>
2019-01-30 20:38:38 -0600

    Made components and updated app component

--- angular/songbird-ng/src/app/app.module.ts
+++ angular/songbird-ng/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MusicianListComponent } from './musician-list/musician-list.component';
+import { EventPageComponent } from './event-page/event-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MusicianListComponent,
+    EventPageComponent
   ],
   imports: [
     BrowserModule
---
+++ angular/songbird-ng/src/app/musician-list/musician-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-musician-list',
+  templateUrl: './musician-list.component.html',
+  styleUrls: ['./musician-list.component.css']
+})
+export class MusicianListComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
